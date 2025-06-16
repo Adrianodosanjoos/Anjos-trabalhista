2ba6d6dda275f57fd0245cd68f4807986cbbb0e1
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-06-16 15:26:10 -0300

    Atualização

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,6 +1,18 @@
 export function Hero(){
     return(
-        <section className="bg-neutral-50">
+        <section className="bg-neutral-50 text-black relative overflow-hidden">
+
+            <div>
+                <article>
+
+                 <div>
+                    Precisa de um especialista <b>para conduzir o seu processo trabalhista</b>
+                 </div>
+
+                 <p>Somos um escritório de advocacia especializado em Direito do Trabalho, com atendimento online em toda Portugal</p>
+
+                </article>
+            </div>
 
         </section>
     )
