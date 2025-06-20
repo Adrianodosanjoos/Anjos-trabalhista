df501c9e3683475e0dfd9858723e44442957f161
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-06-20 13:44:11 -0300

    .

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -24,7 +24,7 @@ export function Hero(){
                 fill
                 sizes='100vw'
                 priority
-                className='object-cover opacity-60'
+                className='object-cover opacity-60 lg:hidden'
                 />
             </div>
 
