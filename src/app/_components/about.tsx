41c0a4de4982da834be82fb5684573f17e5e8f59
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-06-21 12:52:22 -0300

    Atualização

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -7,7 +7,8 @@ export function About() {
       <section className="py-16">
         <div className="container mx-auto grid-cols-1 lg:grid-cols-2 gap-12
         items-end">
-
+            
+            <div className="relative">
             <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
                 <Image
                 src={sobreImg}
@@ -17,6 +18,18 @@ export function About() {
                 className="object-cover hover:scale-105 duration-300"
                 priority
                 />
+            </div>
+
+              <div className="absolute w-40 h-40 right-4 -bottom-8">
+                <Image
+                src={logoImg}
+                alt="Sobre imagem"
+                fill
+                quality={100}
+                priority
+                />
+            </div>
+
             </div>
 
         </div>
