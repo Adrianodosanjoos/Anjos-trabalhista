7e5ee9bd936fa27f0c696819ea80691c2e36d337
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-06-24 12:59:36 -0300

    Atualização

--- src/app/_components/depoimentos.tsx
+++ src/app/_components/depoimentos.tsx
@@ -2,9 +2,17 @@
 
 export function Depoimentos(){
     return( 
-        <section>
+        <section className="py-16">
+            <div className="container mx-auto px-4">
 
-            <h2 className="text-black"> Depoimentos </h2>
+                     <h2 className="text-black text-4xl font-bold mb-12"> Depoimentos </h2>
+                       <div className="relative">
+
+                       </div>
+                
+            </div>
+
+           
 
         </section>
     ) }
