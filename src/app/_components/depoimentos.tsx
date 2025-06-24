d602b3c0814f84b897a852ec9fd8d84d2232e17a
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-06-24 13:36:58 -0300

    Atualização

--- src/app/_components/depoimentos.tsx
+++ src/app/_components/depoimentos.tsx
@@ -1,11 +1,14 @@
+'use client'
 
+import useEmblaCarousel from 'embla-carousel-react'
+import { ChevronLeft, ChevronRight} from 'lucide-react'
 
 export function Depoimentos(){
     return( 
         <section className="py-16">
             <div className="container mx-auto px-4">
 
-                     <h2 className="text-black text-4xl font-bold mb-12"> Depoimentos </h2>
+                     <h2 className="text-black text-4xl font-bold mb-12 text-center"> Depoimentos </h2>
                        <div className="relative">
 
                        </div>
