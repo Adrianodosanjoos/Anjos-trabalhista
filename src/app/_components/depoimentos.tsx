df189a1781b3d7d7cfe80413b38c50824d0dc322
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-06-25 14:59:12 -0300

    Atualização

--- src/app/_components/depoimentos.tsx
+++ src/app/_components/depoimentos.tsx
@@ -155,7 +155,7 @@ export function Depoimentos() {
                                             <div className='relative w-24 h-24'>
                                                 <img src={item.image?.src}
                                                     alt='foto do depoimento'
-                                                       className='relative rounded-full'
+                                                       className='relative rounded-full items-center content-center'
                                                 />
                                 
                                             </div>
