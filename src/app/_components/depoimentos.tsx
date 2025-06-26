96ab471832def0b2bb0147ddaf6f1c5d7f8d24f8
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-06-26 14:47:02 -0300

    .

--- src/app/_components/depoimentos.tsx
+++ src/app/_components/depoimentos.tsx
@@ -181,7 +181,7 @@ export function Depoimentos() {
                     </div>
                 </div>
 
-                <button className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-2 
+                <button className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-6 
                    -translate-y-1/2 -translate-x-1/2 top-5/3 z-10 cursor-pointer'
                     onClick={scrollPrev}
                 >
