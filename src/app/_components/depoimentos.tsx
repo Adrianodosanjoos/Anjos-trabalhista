59989a8dfa70b75b18c41df40acbd325da1907e3
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-06-26 14:42:01 -0300

    Atualização

--- src/app/_components/depoimentos.tsx
+++ src/app/_components/depoimentos.tsx
@@ -191,7 +191,7 @@ export function Depoimentos() {
                 </button>
 
                 <button className='bg-white  flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 
-                   -translate-y-1/2 -translate-x-1/2 top-1/2 z-10 cursor-pointer'
+                   -translate-y-1/2 -translate-x-1/2 top-5/3 z-10 cursor-pointer'
                     onClick={scrollNext}
                 >
                     <ChevronRight
