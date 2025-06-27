b1c82a8bd53f8bc5cfa6888aff830516f3d37724
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-06-27 12:31:26 -0300

    .

--- src/app/_components/depoimentos.tsx
+++ src/app/_components/depoimentos.tsx
@@ -181,7 +181,7 @@ export function Depoimentos() {
                     </div>
                 </div>
 
-                <div className='absolute'>
+                <div className='fixed'>
 
                 <button className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-8 
                    -translate-y-1/2 -translate-x-1/2 top-7/3 md:top-5/3 z-10 cursor-pointer'
