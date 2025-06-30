6dfcb44cb26e91cd3231b94890c86a551897cada
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-06-30 14:13:17 -0300

    .

--- src/app/_components/contactos.tsx
+++ src/app/_components/contactos.tsx
@@ -3,8 +3,22 @@ import { WhatsappLogoIcon } from '@phosphor-icons/react/dist/ssr'
 
 export function Contactos() {
 
-   return (
+   return ( 
+      <div>
     <h1 className="text-3xl text-black font-bold mb-12 flex align-text-center justify-center">Contactos</h1>
+
+    <div>
+       <a href="#"
+                            target="_blank"
+                            className="bg-green-500 px-5 py-2 rounded-md font-semibold 
+                    flex items-center justify-center w-fit gap-2 hover:scale-102 duration-300">
+                            <WhatsappLogoIcon className='w-5 h-5' />
+                            Contacto via WhatApp
+                        </a>
+    </div>
+    </div>
+    
+
    )
 
 
