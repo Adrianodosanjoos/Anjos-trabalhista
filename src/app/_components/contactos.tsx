25ab29579d1a2d44c9756095d7a30fbd163f1698
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-07-01 06:37:21 -0300

    .

--- src/app/_components/contactos.tsx
+++ src/app/_components/contactos.tsx
@@ -22,7 +22,7 @@ export function Contactos() {
             </div>
            
 
-    <h1 className="text-3xl text-black font-bold mb-12 flex align-text-center justify-center">Conte conosco</h1>
+    <h1 className="text-3xl text-black font-bold mb-12 flex align-text-center justify-center z-10">Conte conosco</h1>
 
     <div>
        <a href="#"
