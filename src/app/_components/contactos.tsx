029808a560db0c73c8b8aa80c209c84285fd2808
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-07-01 05:50:43 -0300

    .

--- src/app/_components/contactos.tsx
+++ src/app/_components/contactos.tsx
@@ -8,20 +8,8 @@ export function Contactos() {
    return ( 
       <section className="py-16 relative  max-h-1/2-50px">
       <div>
-    <h1 className="text-3xl text-black font-bold mb-12 flex align-text-center justify-center">Conte conosco</h1>
 
-    <div>
-       <a href="#"
-                            target="_blank"
-                            className="bg-green-500 px-5 py-2 rounded-md font-semibold 
-                    flex items-center justify-center w-fit gap-2 hover:scale-102 duration-300">
-                            <WhatsappLogoIcon className='w-5 h-5' />
-                            Contacto via WhatApp
-                        </a>
-    </div>
-
-          
-               <div>
+                <div>
                 <Image
                 src={logoimg}
                 alt='foto de fundo'
@@ -33,6 +21,18 @@ export function Contactos() {
                   <div className=' absolute inset-0 bg-black  opacity-5 '></div>
             </div>
            
+
+    <h1 className="text-3xl text-black font-bold mb-12 flex align-text-center justify-center">Conte conosco</h1>
+
+    <div>
+       <a href="#"
+                            target="_blank"
+                            className="bg-green-500 px-5 py-2 rounded-md font-semibold 
+                    flex items-center justify-center w-fit gap-2 hover:scale-102 duration-300">
+                            <WhatsappLogoIcon className='w-5 h-5' />
+                            Contacto via WhatApp
+                        </a>
+    </div>
 
     </div>
     
