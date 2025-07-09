4fec60fcf30a7b3de9abb4915ae0c97bccb68c3a
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-07-09 14:15:36 -0300

    Atualização

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -14,6 +14,7 @@ export function Hero(){
                 className='absolute top-0 right-0 w-25 bg-white'
                 quality={100}
                 priority
+                data-aos="fade-right"
                 />
             </div>
 
@@ -36,25 +37,26 @@ export function Hero(){
                 <article className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
 
                  <div className='space-y-6'>
-                    <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold leading-10">
+                    <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold leading-10" data-aos="fade-right">
                     Precisa de um especialista <b>para conduzir o seu processo trabalhista</b>
+                     
                     </h1>
                  
 
-                 <p className="text-lg">Somos um escritório de advocacia especializado em Direito
+                 <p className="text-lg"  data-aos="fade-left">Somos um escritório de advocacia especializado em Direito
                      do Trabalho, com atendimento online em toda Portugal</p>
                       
              
                     <a href={`https://wa.me/351911992009?text= Olá vim pelo site e gostaria de saber como que faço, para abrir um processo contra uma empresa`} 
                     target="_blank"
                     className="bg-green-500 px-5 py-2 rounded-md font-semibold 
-                    flex items-center w-fit gap-2 hover:scale-102 duration-300 text-white">
-                        <WhatsappLogoIcon className='w-5 h-5' />
+                    flex items-center w-fit gap-2 hover:scale-102 duration-300 text-white"  data-aos="fade-right">
+                        <WhatsappLogoIcon className='w-5 h-5'  data-aos="fade-left"/>
                         Contacto via WhatApp
                     </a>
                  </div>
 
-                 <div className='hidden md:block h-full relative'>
+                 <div className='hidden md:block h-full relative'  data-aos="fade-right">
                     <Image
                     src={logoimg}
                     alt='Foto do logo da empresa'
