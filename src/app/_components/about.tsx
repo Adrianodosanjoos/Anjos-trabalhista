60a21ddcc7304ba1e243bf13a9230c0f61ba9b28
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-07-09 14:24:28 -0300

    Atualização

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -22,6 +22,7 @@ export function About() {
                                 quality={100}
                                 className="object-cover hover:scale-105 duration-300"
                                 priority
+                                data-aos="fade-left"
                             />
                         </div>
 
@@ -33,32 +34,33 @@ export function About() {
                                 fill
                                 quality={100}
                                 priority
+                                 data-aos="fade-right"
                             />
                         </div>
 
                     </div>
 
                     <div className="space-y-6 text-black mt-10 px-4">
-                        <h2 className="text-4xl font-bold text-center">Sobre</h2>
+                        <h2 className="text-4xl font-bold text-center" data-aos="fade-left">Sobre</h2>
 
-                        <p>
+                        <p  data-aos="fade-right">
                             Somos um escritório de advocacia especializado em Direito do Trabalho, com atendimento online em toda Portugal
                         </p>
 
                         <ul className="space-y-4">
-                            <li className="flex items-center gap-2">
-                                <Check className="text-red-500 font-bold" />
+                            <li className="flex items-center gap-2" data-aos="fade-left">
+                                <Check className="text-red-500 font-bold"/>
                                 Atendendo deste de 2010
                             </li>
-                            <li className="flex items-center gap-2">
-                                <Check className="text-red-500 font-bold" />
+                            <li className="flex items-center gap-2" data-aos="fade-right">
+                                <Check className="text-red-500 font-bold"/>
                                 Equipe com + 10 Advogados Associados
                             </li>
-                            <li className="flex items-center gap-2">
+                            <li className="flex items-center gap-2"  data-aos="fade-left">
                                 <Check className="text-red-500 font-bold" />
                                 <b>+ 5000</b> Clientes atendidos
                             </li>
-                            <li className="flex items-center gap-2">
+                            <li className="flex items-center gap-2" data-aos="fade-right">
                                 <Check className="text-red-500 font-bold" />
                                 <b>+ 110</b> Depoimentos
                             </li>
@@ -67,7 +69,7 @@ export function About() {
                         <a href={`https://wa.me/351911992009?text= Olá vim pelo site e gostaria de saber como que faço, para abrir um processo contra uma empresa`}
                             target="_blank"
                             className="bg-green-500 px-5 py-2 rounded-md font-semibold 
-                    flex items-center justify-center w-fit gap-2 hover:scale-102 duration-300 text-white">
+                    flex items-center justify-center w-fit gap-2 hover:scale-102 duration-300 text-white" data-aos="fade-left">
                             <WhatsappLogoIcon className='w-5 h-5' />
                             Contacto via WhatApp
                         </a>
--- src/app/_components/contactos.tsx
+++ src/app/_components/contactos.tsx
@@ -25,14 +25,14 @@ export function Contactos() {
    
     <div className='relative flex mx-aut py-2.5 justify-center md:gap-10'>
 
-             <h1 className="text-3xl text-black font-bold text-center">Conte conosco</h1>
+             <h1 className="text-3xl text-black font-bold text-center" data-aos="fade-left">Conte conosco</h1>
 
 
        <a href={`https://wa.me/351911992009?text= Olá vim pelo site e gostaria de saber como que faço, para abrir um processo contra uma empresa`}
                             target="_blank"
                             className="bg-green-500 px-5 py-2 rounded-md font-semibold 
                     items-center justify-center w-fit gap-2 hover:scale-102 duration-300 opacity-80 flex">
-                            <WhatsappLogoIcon className='w-8 h-8 md:w-5 md:h-5' />
+                            <WhatsappLogoIcon className='w-8 h-8 md:w-5 md:h-5' data-aos="fade-right"/>
                             Contacto via WhatApp
                         </a>
     </div>
